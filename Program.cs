092d1c6026a605f6f7d7f8cf3dbac35ec537cc92
ArseniiSilaev <Senyasilaev@yandex.ru>
2022-01-23 13:11:27 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите Вашу строку: ");
+            string userString = Console.ReadLine();
+            int repeatCount = 8;
+            while (repeatCount-- > 0)
+            {
+                Console.WriteLine(userString);
+            }
+        }
+    }
+}
