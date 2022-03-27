4113ec5f2df3c24fe5e33a1d51458858106a3048
ArseniiSilaev <96460548+ArseniiSilaev@users.noreply.github.com>
2022-03-27 18:58:30 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,24 +6,19 @@ using System.Threading.Tasks;
 
 namespace homework2
 {
-    
     internal class Program
     {
-        
         static void Main(string[] args)
         {
             Console.Write("Введите Вашу строку: ");
             string userString = Console.ReadLine();
             Console.Write("Введите количество повторений: ");
             int repeatCount = Convert.ToInt32(Console.ReadLine());
-            
+
             for (int i = 0; i < repeatCount; i++)
             {
                 Console.WriteLine(userString);
             }
-            
         }
-        
     }
-    
 }
